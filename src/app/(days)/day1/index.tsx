430044e3cbf5f7d78fd5d122417ca0fb5aa92a96
mariosknl <mariosknl@outlook.com>
2024-04-09 12:19:37 +0300

    setup expo router & the main screens

---
+++ src/app/(days)/day1/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const DayDetailsScreen = () => {
+	return (
+		<View>
+			<Text>Day Details Screen</Text>
+		</View>
+	);
+};
+
+export default DayDetailsScreen;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import {
-	StyleSheet,
-	Text,
-	View,
-	FlatList,
-	ActivityIndicator,
-} from "react-native";
-import DayListItem from "./src/components/core/DayListItem";
+import { FlatList, StyleSheet, View } from "react-native";
 
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 import {
 	AmaticSC_400Regular,
 	AmaticSC_700Bold,
 } from "@expo-google-fonts/amatic-sc";
+import { Inter_900Black, useFonts } from "@expo-google-fonts/inter";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import DayListItem from "../components/core/DayListItem";
 
 SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
-export default function App() {
+export default function HomeScreen() {
 	let [fontsLoaded, fontError] = useFonts({
 		Inter: Inter_900Black,
 		Amatic: AmaticSC_400Regular,
