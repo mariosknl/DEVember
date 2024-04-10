c311d87364ffe5d5eb90ac3bd5b8fabb8b513e7a
mariosknl <mariosknl@outlook.com>
2024-04-10 17:20:39 +0300

    utilizing user location data to fetch and display weather data

--- src/app/(days)/day8/weather.tsx
+++ src/app/(days)/day8/weather.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { useEffect, useState } from "react";
 import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import * as Location from "expo-location";
 
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
 const OPEN_WEATHER_KEY = process.env.EXPO_PUBLIC_OPEN_WEATHER_KEY;
@@ -18,21 +19,39 @@ type WeatherProps = {
 };
 const WeatherScreen = () => {
 	const [weather, setWeather] = useState<WeatherProps>();
+	const [location, setLocation] = useState<Location.LocationObject>();
+	const [errorMsg, setErrorMsg] = useState("");
+
+	useEffect(() => {
+		if (location) {
+			fetchWeather();
+		}
+	}, [location]);
+
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				setErrorMsg("Permission to access location was denied");
+				return;
+			}
+
+			let location = await Location.getCurrentPositionAsync({});
+			console.log(location);
+			setLocation(location);
+		})();
+	}, []);
 
 	// fetch data
 	const fetchWeather = async () => {
-		const lat = 38.2466;
-		const lon = 21.7346;
+		if (!location) return;
+
 		const results = await fetch(
-			`${BASE_URL}?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_KEY}&units=metric`
+			`${BASE_URL}?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${OPEN_WEATHER_KEY}&units=metric`
 		);
 		const data = await results.json();
 		setWeather(data);
 	};
-
-	useEffect(() => {
-		fetchWeather();
-	}, []);
 
 	if (!weather) {
 		return <ActivityIndicator />;
