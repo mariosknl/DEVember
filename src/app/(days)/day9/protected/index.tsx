f9e0e8cfb9f62f74976f6a2f8474f5fc1fca2bcc
mariosknl <mariosknl@outlook.com>
2024-04-25 13:37:36 +0300

    implementing auth services from aws

--- src/app/(days)/day9/protected/index.tsx
+++ src/app/(days)/day9/protected/index.tsx
@@ -1,11 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { useAuthenticator } from "@aws-amplify/ui-react-native";
+
 const ProtectedScreen = () => {
+	const { signOut } = useAuthenticator();
 	return (
 		<View style={{ padding: 10 }}>
 			<Text style={{ fontFamily: "InterBold", fontSize: 30 }}>Hello there</Text>
 			<Text style={{ fontFamily: "InterSemi", fontSize: 30, color: "gray" }}>
 				You should see this page only if you are authenticated
 			</Text>
+
+			<Button title="Sign out" onPress={() => signOut()} />
 		</View>
 	);
 };
